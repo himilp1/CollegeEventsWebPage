413b09b0cee39029ec9ace0613dd96d404edcd27
Nick <nickgr1052@gmail.com>
2023-03-31 15:03:52 -0400

    Current Work

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-#testing 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+cred = credentials.Certificate("firebase_sdk.json")
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/cop4710'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,25 +32,39 @@ def hello():
     return render_template('index.html')
     
     
-@app.route('/', methods = ['POST'])
-def login():
+@app.route('/login', methods = ['POST'])
+def login_post():
 
     username = request['username']
     password = request['password']
 
-
-
-@app.route('/registerUser')
-def login_pos():
-    role = request['role']
-    username = 'Nick1052'
-    password = 'test123'
-    email = 'Nick1052@gmail.com'
     
 
+@app.route('/register')
+def register_post():
+    
+    password = request.form['password']
+    email = request.form['email']
+
+    
+    try:
+        user = auth.create_user(
+            email = email,
+            password = password
+        )
+
+        return jsonify({
+                        "Success": True,
+                        "Message": "User was able to registered"
+                       })
+    
+    except:
+        return jsonify({
+                        "Success": False,
+                        "Message": "User not able to registered"
+                       })
 
     new_user = Users(username, password, email)
-
     db.session.add(new_user)
     db.session.commit()
 
